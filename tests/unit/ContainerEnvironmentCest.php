5dbb5d75f610c6e1c1a6ecd4408b6060aad8f479
deefernando6 <deefernando6@gmail.com>
2023-10-31 11:06:46 +0530

    update ubuntu, mariadb versions

--- tests/unit/ContainerEnvironmentCest.php
+++ tests/unit/ContainerEnvironmentCest.php
@@ -9,16 +9,16 @@ class ContainerEnvironmentCest
     // tests
     public function vefiryOSVersionTest (UnitTester $I)
     {
-        $I->wantTo("Veify the container os version - Ubuntu 20.04");
+        $I->wantTo("Veify the container os version - Ubuntu 22.04");
         $I->runShellCommand("docker exec db_container cat /etc/os-release | grep PRETTY_NAME | cut -d'=' -f 2"); 
-        $I->seeInShellOutput("Ubuntu 20.04.3 LTS");
+        $I->seeInShellOutput("Ubuntu 22.04 LTS");
     }
 
     public function vefiryInstalledMariaDBVersionTest (UnitTester $I)
     {
-        $I->wantTo("Veify the MariaDB version - 10.4");
+        $I->wantTo("Veify the MariaDB version - 10.11.5");
         $I->runShellCommand("docker exec db_container mysql -uroot -p1234 --execute=\"SELECT @@version;\""); 
-        $I->seeInShellOutput("10.4.22");
+        $I->seeInShellOutput("10.11.5");
     }
 
     public function vefiryInstalledMariaDBRepoTest (UnitTester $I)
